d8da3a4ecf45c7fad0dc9bbfa7393f8da83ab47c
Karim Roy tampubolon <86301908+kajukopi@users.noreply.github.com>
2025-06-30 18:32:27 +0700

    Update firmware.ino

--- firmware.ino
+++ firmware.ino
@@ -1,5 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <ArduinoOTA.h>
 #include <ESP8266HTTPUpdateServer.h>
 
 const char* ssid = "karimroy";
@@ -8,68 +9,65 @@ const char* password = "09871234";
 ESP8266WebServer server(80);
 ESP8266HTTPUpdateServer httpUpdater;
 
-const int ledPin = LED_BUILTIN;
-bool ledState = false;
+const int ledPin = 2; // Onboard LED
 
-const char html[] PROGMEM = R"rawliteral(
-<!DOCTYPE html>
-<html>
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <style>
-    body { font-family: sans-serif; background: #f9f9f9; text-align: center; padding: 20px; }
-    button { font-size: 20px; padding: 10px 30px; margin: 10px; }
-    input[type=file], input[type=submit] { margin-top: 15px; font-size: 16px; }
-  </style>
-</head>
-<body>
-  <h2>NodeMCU Control</h2>
-  <button onclick="toggleLED()">Toggle LED</button><br><br>
-  <form method="POST" action="/update" enctype="multipart/form-data">
-    <input type="file" name="update"><br>
-    <input type="submit" value="Update Firmware">
-  </form>
-  <script>
-    function toggleLED() {
-      fetch('/toggle')
-        .then(res => res.text())
-        .then(txt => alert('LED is ' + txt));
-    }
-  </script>
-</body>
-</html>
-)rawliteral";
+void handleRoot() {
+  String html = R"rawliteral(
+    <h1>NodeMCU LED Control</h1>
+    <p>LED is <b>%STATE%</b></p>
+    <a href="/led/on"><button>Turn ON</button></a>
+    <a href="/led/off"><button>Turn OFF</button></a>
+    <p><a href="/update">OTA Firmware Upload</a></p>
+  )rawliteral";
+
+  html.replace("%STATE%", digitalRead(ledPin) == LOW ? "ON" : "OFF");
+  server.send(200, "text/html", html);
+}
+
+void handleLedOn() {
+  digitalWrite(ledPin, LOW); // LED ON (active LOW)
+  server.sendHeader("Location", "/");
+  server.send(303);
+}
+
+void handleLedOff() {
+  digitalWrite(ledPin, HIGH); // LED OFF
+  server.sendHeader("Location", "/");
+  server.send(303);
+}
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, HIGH);  // LED OFF
-
   Serial.begin(115200);
   WiFi.begin(ssid, password);
-  Serial.print("Connecting");
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH); // Start with LED OFF
+
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500); Serial.print(".");
+    delay(500);
+    Serial.print(".");
   }
-  Serial.println("\nConnected to WiFi");
-  Serial.print("IP: ");
+
+  Serial.println();
+  Serial.println("WiFi connected at: ");
   Serial.println(WiFi.localIP());
 
-  server.on("/", HTTP_GET, []() {
-    server.send_P(200, "text/html", html);
-  });
+  // Web server routes
+  server.on("/", handleRoot);
+  server.on("/led/on", handleLedOn);
+  server.on("/led/off", handleLedOff);
 
-  server.on("/toggle", HTTP_GET, []() {
-    ledState = !ledState;
-    digitalWrite(ledPin, ledState ? LOW : HIGH);
-    server.send(200, "text/plain", ledState ? "ON" : "OFF");
-  });
+  // OTA firmware page
+  httpUpdater.setup(&server); // /update
 
-  httpUpdater.setup(&server);
   server.begin();
+  Serial.println("HTTP server started");
+
+  ArduinoOTA.setHostname("NodeMCU-OTA");
+  ArduinoOTA.begin();
+  Serial.println("OTA Ready");
 }
 
 void loop() {
   server.handleClient();
+  ArduinoOTA.handle();
 }
-
-// Edit Files 2
