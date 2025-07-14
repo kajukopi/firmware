ca10e56f22059ffacff401b408ab773238d298e3
Karim Roy tampubolon <86301908+kajukopi@users.noreply.github.com>
2025-07-15 00:34:06 +0700

    Update tokens.h

--- sketch/tokens.h
+++ sketch/tokens.h
@@ -1,5 +1,8 @@
-#define WIFI_SSID     "karimroy"
+#ifndef TOKENS_H
+#define TOKENS_H
+
+// WiFi
+#define WIFI_SSID "karimroy"
 #define WIFI_PASSWORD "09871234"
 
-#define BOT_TOKEN     "7826449058:AAHZgPVpCdmwHsmCO6D9wIsnpZj3fOyjXWM"
-#define CHAT_ID       "7891041281"
+#endif
