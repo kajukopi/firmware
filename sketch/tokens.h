0b16c8cd5997cbfbeb251955bcdf948edb1cd9ae
Karim Roy tampubolon <86301908+kajukopi@users.noreply.github.com>
2025-07-15 11:42:57 +0700

    Update tokens.h

--- sketch/tokens.h
+++ sketch/tokens.h
@@ -1,2 +1,12 @@
-#define WIFI_SSID "karimroy"
-#define WIFI_PASSWORD "09871234"
+#ifndef TOKENS_H
+#define TOKENS_H
+
+#define WIFI_SSID       "karimroy"
+#define WIFI_PASSWORD   "09871234"
+
+#define API_KEY         "AIzaSyBczsujBWZbP2eq5C1YR1JF3xPixWVYnxY"
+#define DATABASE_URL    "https://payunghitam-default-rtdb.asia-southeast1.firebasedatabase.app"
+#define USER_EMAIL      "esp32@payunghitam.com"
+#define USER_PASSWORD   "esp32admin"
+
+#endif
